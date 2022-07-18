a8e18a9a77285ca84e9349cbfbf8bd5aa67f188b
Onwuesi Williams <onwuesiwilliamsemeka@gmail.com>
2022-07-18 04:45:27 -0700

    williams changes to student navigation links

--- src/components/layouts/app-layout/navigations/studentNavs.js
+++ src/components/layouts/app-layout/navigations/studentNavs.js
@@ -8,38 +8,69 @@ export default [
     header: "Students",
   },
   {
-    id: "email",
-    title: "Dine",
-    icon: <Icon icon = "fluent:mail-24-filled"/>,
-    navLink: "/student/courses",
-  },
-  {
-    id: "chat",
-    title: "Chat",
-    icon: <Icon icon="heroicons-solid:chat-alt-2" />,
-    navLink: "/student/courses",
-  },
-  {
     id: "dashboards",
     title: "Dashboards",
     icon: <Icon icon="ep:menu" />,
-    badge: "light-warning",
-    badgeText: "2",
-    children: [
-      {
-        id: "analyticsDash",
-        title: "Analytics",
-        icon: <Circle size={12} />,
-        navLink: "/student/courses",
-      },
-      {
-        id: "eCommerceDash",
-        title: "eCommerce",
-        icon: <Circle size={12} />,
-        navLink: "/student/courses",
-      },
-    ],
+    navLink: "/student/courses",
   },
+  {
+    id: "courses",
+    title: "Courses",
+    icon: <Icon icon="bi:collection-fill" />,
+    navLink: "/student/courses",
+  },
+  {
+    id: "downloads",
+    title: "Downloads",
+    icon: <Icon icon="akar-icons:download" />,
+    navLink: "/student/courses",
+  },
+  {
+    id: "tution&clearance",
+    title: "Tution & Clearance",
+    icon: <Icon icon="fa-regular:credit-card" />,
+    navLink: "/student/courses",
+  },
+  {
+    id: "events",
+    title: "Events",
+    icon: <Icon icon="bi:calendar2-event-fill" />,
+    navLink: "/student/courses",
+  },
+  {
+    id: "profile",
+    title: "Profile",
+    icon: <Icon icon="icomoon-free:profile" />,
+    navLink: "/student/courses",
+  },
+  {
+    id: "helpdesk",
+    title: "Help Desk",
+    icon: <Icon icon="ri:customer-service-2-line" />,
+    navLink: "/student/courses",
+  },
+
+  //{
+    //id: "dashboards",
+    //title: "Dashboards",
+    //icon: <Icon icon="ep:menu" />,
+    //badge: "light-warning",
+    //badgeText: "2",
+    //children: [
+      //{
+        //id: "analyticsDash",
+        //title: "Analytics",
+        //icon: <Circle size={12} />,
+        //navLink: "/student/courses",
+      //},
+      //{
+        //id: "eCommerceDash",
+        //title: "eCommerce",
+        //icon: <Circle size={12} />,
+        //navLink: "/student/courses",
+      //},
+    //],
+  //},
   {
     header: "Partners",
   },
