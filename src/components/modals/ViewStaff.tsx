056097d9a3e3f403bf189b82e3a5dbb2338d4861
Solomon <doziben@gmail.com>
2023-04-10 10:47:52 +0100

    fix: viewing students modal

--- src/components/modals/ViewStaff.tsx
+++ src/components/modals/ViewStaff.tsx
@@ -7,21 +7,22 @@ import FormInput from "../molecules/FormInput";
 
 interface ViewStaffProps {
   data: any;
+  isStudent?: boolean;
 }
 
-export default function ViewStaff({ data }: ViewStaffProps) {
+export default function ViewStaff({ data, isStudent }: ViewStaffProps) {
   const [visibility, toggle] = useToggle();
 
   const initialState = {
     name: data?.name,
     email: data?.email,
     phone: data?.phone,
-    gender: data?.gender,
+    gender: data?.gender ?? "Not provided",
     role: data?.role,
     privilege: data?.privilege,
-    birthDate: data?.birthDate,
-    state: data?.state,
-    city: data?.city,
+    birthDate: data?.birthDate ?? "Not provided",
+    state: data?.state ?? "Not provided",
+    city: data?.city ?? "Not provided",
   };
 
   const { formData, updateForm } = useForm({ initialState });
@@ -46,7 +47,23 @@ export default function ViewStaff({ data }: ViewStaffProps) {
           <form className="mt-3" onSubmit={onSubmit}>
             <FormInput label="Name" value={formData.name} disabled />
             <FormInput label="Email" value={formData.email} disabled />
-            {/* <FormInput label="Birth Date" value={new Date(formData.birthDate)?.toDateString()} disabled /> */}
+            {isStudent && (
+              <>
+                <FormInput
+                  label="Phone number"
+                  value={formData.phone}
+                  disabled
+                />
+                <FormInput
+                  label="Birth Date"
+                  value={new Date(formData.birthDate)?.toDateString()}
+                  disabled
+                />
+                <FormInput label="Gender" value={formData.gender} disabled />
+                <FormInput label="State" value={formData.state} disabled />
+                <FormInput label="City" value={formData.city} disabled />
+              </>
+            )}
 
             {/* 
             <button
