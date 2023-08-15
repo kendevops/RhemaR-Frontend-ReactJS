3cb79fafdc2f272343a71d2844b907c45e169f86
lukechidubem <lukechidubem@gmail.com>
2023-08-15 08:26:19 +0100

    fixed bugs on admin

--- src/components/general/table/Table.tsx
+++ src/components/general/table/Table.tsx
@@ -90,14 +90,8 @@ export default function Table({ columns, data, itemsPerPage }: TableProps) {
   return (
     <section>
       <table className="table caption-top text-nowrap">
-        <thead className="">
-          <tr
-            style={{
-              color: "black",
-            }}
-          >
-            {colHeaders}
-          </tr>
+        <thead className="bg-blue-800">
+          <tr>{colHeaders}</tr>
         </thead>
 
         <tbody>{tabData}</tbody>
