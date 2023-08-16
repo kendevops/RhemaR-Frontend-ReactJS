a09dddc78764cf38d9930a142e131f319a7cc8e5
Kenneth <kenneth.aladi@xische.com>
2023-08-16 18:56:41 +0100

    fixed academic managment

--- src/views/ict-admin/AcademicManagement.tsx
+++ src/views/ict-admin/AcademicManagement.tsx
@@ -289,15 +289,13 @@ export default function AcademicManager() {
         </div>
       )}
 
-          <button
-            className="btn btn-blue-800 btn-lg d-flex gap-3 my-3   align-items-center "
-            style={{ width: "fit-content" }}
-          >
-            <HiPlus />
-            Export Campus List
-          </button>
-        </article>
-      </div>
+      <button
+        className="btn btn-blue-800 btn-lg d-flex gap-3 my-3   align-items-center "
+        style={{ width: "fit-content" }}
+      >
+        <HiPlus />
+        Export Campus List
+      </button>
 
       {/* level */}
 
