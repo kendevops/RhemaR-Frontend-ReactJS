b9a51c65fa01442fefd9e9bd78fd579ce65c6e1b
lukechidubem <lukechidubem@gmail.com>
2023-08-16 08:36:25 +0100

    worked on users with multiple roles

--- src/views/ict-admin/AcademicManagement.tsx
+++ src/views/ict-admin/AcademicManagement.tsx
@@ -286,6 +286,14 @@ export default function AcademicManager() {
               toggle={toggle}
             />
           </main>
+
+          <button
+            className="btn btn-blue-800 btn-lg d-flex gap-3 my-3   align-items-center "
+            style={{ width: "fit-content" }}
+          >
+            <HiPlus />
+            Export Campus List
+          </button>
         </div>
       )}
 
