ff8818c882eed8f16df0e2b137ea82c7f0a1c429
Kenneth <kendevops@gmail.com>
2023-10-23 11:25:15 +0100

    added queryclient with defaultOptions

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 // ** Router Import
-import SocketWrapper from "./components/layouts/SocketWrapper";
+// import SocketWrapper from "./components/layouts/SocketWrapper";
 import Router from "./router/Router";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RoleContextProvider } from "./utility/context/roleContext";
 import { AccessContextProvider } from "./utility/context/accessContext";
-const queryclient = new QueryClient();
+// const queryclient = new QueryClient();
+
+const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
+const queryclient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnmount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: twentyFourHoursInMs,
+    },
+  },
+});
 
 const App = () => (
   <QueryClientProvider client={queryclient}>
     <RoleContextProvider>
       <AccessContextProvider>
-        <SocketWrapper>
+        {/* <SocketWrapper> */}
           <Router />
-        </SocketWrapper>
+        {/* </SocketWrapper> */}
       </AccessContextProvider>
     </RoleContextProvider>
   </QueryClientProvider>
